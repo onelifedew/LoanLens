22743671d2ed8601e922d8e67f0212f2415f1879
onelifedew <LIFEDEW137@GMAIL.COM>
2026-01-15 14:32:56 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,20 @@
 import dotenv from "dotenv";
+import "@nomicfoundation/hardhat-toolbox";
 
 dotenv.config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 const config = {
   solidity: "0.8.20",
   networks: {
     mantleSepolia: {
-      type: "http",
       url: process.env.MANTLE_SEPOLIA_RPC_URL || "https://mantle-sepolia.drpc.org",
       chainId: 5003,
-      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
     },
   },
 };
 
 export default config;
-
